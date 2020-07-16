d179796d251815b204349335b183cc6a5cf5fe00
praz <t.prazwal@gmail.com>
2020-07-16 11:24:38 +0200

    add post model and associtaion

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_112641) do
+ActiveRecord::Schema.define(version: 2020_07_16_090434) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -27,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_07_15_112641) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
