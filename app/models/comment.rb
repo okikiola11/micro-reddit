cde3fca4d5ba1cc3635d819dd3f731972125d5e8
praz <t.prazwal@gmail.com>
2020-07-16 12:51:29 +0200

    add comment model and associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
-class Post < ApplicationRecord
+class Comment < ApplicationRecord
+  belongs_to :post
   belongs_to :user
-  has_many :comments
   validates :title, :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_090434) do
+ActiveRecord::Schema.define(version: 2020_07_16_095628) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -27,5 +38,7 @@ ActiveRecord::Schema.define(version: 2020_07_16_090434) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
